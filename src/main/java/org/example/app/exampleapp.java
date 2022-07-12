6c4e6e85b647dde52c4c8228da0e41b097a5dfc6
Guy Balmas <guy.balmas@gmail.com>
2022-07-12 23:08:03 +0300

    + try fix image name

--- src/main/java/org/example/app/exampleapp.java
+++ src/main/java/org/example/app/exampleapp.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class exampleapp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+//		System.out.println("BP_OCI_VERSION = " + System.getenv("BP_OCI_VERSION"));
 		SpringApplication.run(exampleapp.class, args);
 	}
 
